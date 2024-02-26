9fe3db7dfd01e6422290f57c5b024f06c8419f60
skapskap <skaprange@gmail.com>
2024-02-26 20:22:48 -0300

    feat: mobile navbar implemented

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -8,8 +8,6 @@ import {
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import Image from "next/image";
