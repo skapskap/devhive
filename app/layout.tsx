81bef37679335d5ea4617cb95931db4113508154
skapskap <skaprange@gmail.com>
2024-03-07 15:06:03 -0300

    assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
   title: "DevHive",
   description:
     "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
-  icons: "/assets/images/site-logo.svg",
+  icons: "/assets/images/site-logo.png",
 };
 
 export default function RootLayout({
