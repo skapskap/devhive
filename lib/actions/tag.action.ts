ae702ad6a02c6fd5e8b57869d39e58fea0248e7f
skapskap <skaprange@gmail.com>
2024-03-11 11:14:33 -0300

    add tags overview page

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tags.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   // eslint-disable-next-line no-empty
@@ -23,6 +24,19 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "Next.js" },
       { _id: "3", name: "Django" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
