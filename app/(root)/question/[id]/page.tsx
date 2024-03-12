49d57739dd0060766da6fa1d0297fdcde4e42580
skapskap <skaprange@gmail.com>
2024-03-12 16:58:42 -0300

    votes component

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -3,6 +3,7 @@ import AllAnswers from "@/components/shared/AllAnswers";
 import Metric from "@/components/shared/Metric";
 import ParseHTML from "@/components/shared/ParseHTML";
 import RenderTag from "@/components/shared/RenderTag";
+import Votes from "@/components/shared/Votes";
 import { getQuestionById } from "@/lib/actions/question.action";
 import { getUserById } from "@/lib/actions/user.action";
 import { formatNumber, getTimestamp } from "@/lib/utils";
@@ -51,7 +52,7 @@ const QuestionDetails = async ({ params, searchParams }: Props) => {
             </p>
           </Link>
           <div className="flex justify-end">
-            {/* TODO: Making voting functionality VOTING */}
+            <Votes />
           </div>
         </div>
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
